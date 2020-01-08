569dcb203659b9b049d3d8b9a7753423a03e73d3
jlmeister <iamphil4483@yahoo.com>
2020-01-08 15:47:17 -0600

    created button with state

--- src/App.js
+++ src/App.js
@@ -2,25 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isclicked: false
+  };
+  toggleClick = () => {
+    this.setState({
+      isclicked: !this.state.isclicked
+    })
+  };
+  render() {
+    return (
+      <div className="App-header">
+        <button onClick={this.toggleClick}>{this.state.isclicked.toString()}</button>
+      </div>
+    )
+  }
 }
 
 export default App;
