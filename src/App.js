4ddf756f337448bb8a46a4ab5b38147c5675739c
jlmeister <iamphil4483@yahoo.com>
2020-01-08 17:19:09 -0600

    functional todo list without the ability to remove list items. will look at that next.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const TodoList = ({ list }) => {
+  return (
+    <div>
+      {list.map((todo, index) => {
+        return (
+          <div key={index} className="list-item">
+            <p>{todo}</p>
+            {/* add button to remove list item (use 'this' somehow) */}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
 class App extends React.Component {
   state = {
-    isclicked: false
+    input: '',
+    todos: []
   };
-  toggleClick = () => {
+  formSubmit = event => {
+    event.preventDefault();
     this.setState({
-      isclicked: !this.state.isclicked
+      todos: this.state.todos.concat(this.state.input),
+      input: ''
+    })
+  }
+  inputUpdate = event => {
+    this.setState({
+      input: event.target.value
     })
   };
   render() {
     return (
       <div className="App-header">
-        <button onClick={this.toggleClick}>{this.state.isclicked.toString()}</button>
+        <TodoList list={this.state.todos}/>
+        <form onSubmit={this.formSubmit}>
+          <input value={this.state.input} onChange={this.inputUpdate}/>
+          <button>submit</button>
+        </form>
       </div>
     )
   }
