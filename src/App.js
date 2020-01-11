d62512a482f1ba27967ba7198e18497fe605691b
jlmeister <iamphil4483@yahoo.com>
2020-01-11 03:50:38 -0600

    finishing touches. added TodoList function component. final commit for assignment

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// const TodoList = ({ list, onDelete }) => {
-//   return (
-//     <div className="list-container">
-//       {list.map((todo, index) => {
-//         return (
-//           <div key={index} className="list-item">
-//             <p>{todo}</p>
-//             <button onClick={() => onDelete(index)}>X</button>
-//           </div>
-//         )
-//       })}
-//     </div>
-//   )
-// }
+const TodoList = (props) => {
+  const { list, ...other } = props;
+  return (
+    <div className="list-container">
+      {props.list.map((todo, i) => {
+        return (
+          <Todo {...other} entry={todo} key={i} index={i} onDelete={props.onDelete} />
+        )
+      })}
+    </div>
+  )
+}
 
 class Todo extends React.Component {
   constructor(props) {
@@ -30,6 +28,7 @@ class Todo extends React.Component {
     })
   }
   saveEditedEntry = () => {
+    // only allow it to be saved if the entry isn't an empty string
     if (this.refs.editText.value.length > 0) {
       this.setState({
         isInEditMode: false
@@ -38,24 +37,25 @@ class Todo extends React.Component {
     }
   }
   onKeyUp = (event) => {
-    if (event.keyCode === 13)
+    if (event.keyCode === 13) // 'RETURN' key
       return this.saveEditedEntry();
-    if (event.keyCode === 27)
+    if (event.keyCode === 27) // 'ESC' key
       this.changeEditMode();
   }
 
   render() {
     return (
       this.state.isInEditMode ?
-        <div key={this.props.index} className="list-item">
-          <input type="text" defaultValue={this.props.entry} ref="editText" onKeyUp={this.onKeyUp} autoFocus="true"/>
-          <button onClick={this.changeEditMode}>X</button>
+        <div className="list-item">
+          <input type="text" defaultValue={this.props.entry} ref="editText" onKeyUp={this.onKeyUp} autoFocus={true} />
+          <button onClick={this.changeEditMode}>cancel</button>
           <button onClick={this.saveEditedEntry}>OK</button>
         </div>
         :
-        <div key={this.props.index} className="list-item">
+        <div className="list-item">
           <p onDoubleClick={this.changeEditMode}>{this.props.entry}</p>
-          <button onClick={() => this.props.onDelete(this.props.index)}>X</button>
+          <button onClick={this.changeEditMode}>edit</button>
+          <button onClick={() => this.props.onDelete(this.props.index)}>delete</button>
         </div>
     )
   }
@@ -78,7 +78,7 @@ class App extends React.Component {
       this.setState({
         todos: this.state.todos.concat(this.state.input),
         input: ''
-      })     
+      })
     }
   }
   inputUpdate = event => {
@@ -105,13 +105,10 @@ class App extends React.Component {
       <div className="App-header">
         <h1>Let's make a To-Do List</h1>
         <form onSubmit={this.formSubmit}>
-          <input value={this.state.input} onChange={this.inputUpdate} placeholder="type todo here"/>
+          <input value={this.state.input} onChange={this.inputUpdate} placeholder="type todo here" />
           <button>submit</button>
         </form>
-        <div className="list-container">
-          {this.state.todos.map((todo, i) => <Todo entry={todo} index={i} onDelete={this.onDelete} updateEntry={this.updateTodo}/>)}
-        </div>
-        {/* <TodoList list={this.state.todos} onDelete={this.onDelete} /> */}
+        <TodoList list={this.state.todos} onDelete={this.onDelete} updateEntry={this.updateTodo} />
       </div>
     )
   }
